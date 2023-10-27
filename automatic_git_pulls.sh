58ea787c72f8463b883c4ac70e72a54556476a46
sophiacarlone <sophiamcar@gmail.com>
2023-10-27 16:28:23 -0400

    can find all the .gits

--- automatic_git_pulls.sh
+++ automatic_git_pulls.sh
@@ -1,17 +1,11 @@
 #! /bin/bash
 
-echo "pulling"
-find . -name '.git'
-echo "oof"
+echo "pulling repos"
 #go through all the directories in user's home
 for i in $(find . -name '.git')
 do
-	echo $i
-	#if[[i != *cache* ]]; 
-	#then
-		#echo i
-		#cd 
-		#git fetch
-		#git pull
-	#fi
+	if [ $i != *cache* ]; then
+		echo $i
+	fi
+
 done
