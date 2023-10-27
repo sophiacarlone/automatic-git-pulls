530f4820baee08ca19015de7ccbdac4166104271
sophiacarlone <sophiamcar@gmail.com>
2023-10-27 16:42:17 -0400

    finding personal gits

--- automatic_git_pulls.sh
+++ automatic_git_pulls.sh
@@ -4,7 +4,7 @@ echo "pulling repos"
 #go through all the directories in user's home
 for i in $(find . -name '.git')
 do
-	if [ $i != *cache* ]; then
+	if [[ $i != *cache* ]] && [[ $i != *yay* ]]; then
 		echo $i
 	fi
 
