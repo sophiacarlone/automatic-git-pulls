ff305840135477f0a7b797ae9a56630794ba3d15
Sophia Carlone <71948392+sophiacarlone@users.noreply.github.com>
2023-10-27 18:19:38 -0400

    Update for testing purposes

--- automatic_git_pulls.sh
+++ automatic_git_pulls.sh
@@ -1,18 +1,16 @@
 #! /bin/bash
 
 echo "pulling repos"
-#go through all the directories in user's home
+#could go through all the directories in user's home
 for i in $(find . -name '.git')
 do
 	if [[ $i != *cache* ]] && [[ $i != *yay* ]]; then
-		echo $i
+		#echo $i
 		name=${i::-5}
 		echo $name
 		cd $name
 		git fetch
 		git pull
 		cd -
-
 	fi
-
 done
