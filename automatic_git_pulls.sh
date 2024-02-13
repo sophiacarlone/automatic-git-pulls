bc4e7064c87dab407c8e15145954b64ae4c8e487
sophiacarlone <sophiamcar@gmail.com>
2024-02-12 21:34:27 -0500

    upgrades

--- automatic_git_pulls.sh
+++ automatic_git_pulls.sh
@@ -4,13 +4,19 @@ echo "pulling repos"
 #could go through all the directories in user's home
 for i in $(find . -name '.git')
 do
-	if [[ $i != *cache* ]] && [[ $i != *yay* ]]; then
-		#echo $i
+	if [ $i != *cache* ] && [ $i != *yay* ]; then
 		name=${i::-5}
 		echo $name
 		cd $name
+		OUTPUT=$(git status)
 		git fetch
-		git pull
+		#this works for some reason
+		if [ "$OUTPUT" == *"Your branch is behind"* ]; then
+			git pull	
+		elif [ "$OUTPUT" != *"up to date with"* ] && [ "$OUTPUT" != *"Changes not staged for commit"* ]; then
+			echo "Possible commits"
+		fi
 		cd -
+		echo " "
 	fi
 done
